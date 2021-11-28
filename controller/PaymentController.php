3c3aedc8549a7d9859befefcc143920c93d19bb8
Sebastian <sebastianruedaolarte@gmail.com>
2021-11-28 12:23:35 -0300

    -

--- controller/PaymentController.php
+++ controller/PaymentController.php
@@ -70,35 +70,4 @@ class PaymentController extends BaseController {
 
         echo $this->printer->render("view/paymentView.html", $data);
     }
-
-    public function pay() {
-        require_once 'third-party/mercado-pago-sdk/vendor/autoload.php';
-
-        MercadoPago\SDK::setAccessToken("TEST-3535777700995931-112622-0ba52566bab7d024c496f58b13e6e3e1-277641756");
-
-        $payment = new MercadoPago\Payment();
-        $payment->transaction_amount = (float)$_POST['transactionAmount'];
-        $payment->token = $_POST['token'];
-        $payment->description = $_POST['description'];
-        $payment->installments = (int)$_POST['installments'];
-        $payment->payment_method_id = $_POST['paymentMethodId'];
-        $payment->issuer_id = (int)$_POST['issuer'];
-
-        $payer = new MercadoPago\Payer();
-        $payer->email = $_POST['email'];
-        $payer->identification = array(
-            "type" => $_POST['docType'],
-            "number" => $_POST['docNumber']
-        );
-        $payment->payer = $payer;
-
-        $payment->save();
-
-        $response = array(
-            'status' => $payment->status,
-            'status_detail' => $payment->status_detail,
-            'id' => $payment->id
-        );
-        echo json_encode($response);
-    }
 }
